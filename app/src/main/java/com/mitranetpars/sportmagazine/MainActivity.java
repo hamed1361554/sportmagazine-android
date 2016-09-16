0fc402ec84b68d9817fa01a2c4a1581cb054649e
hamed1361554 <hamed.zekri@gmail.com>
2016-09-16 11:47:34 +0330

    Reference on realm database got added.

--- app/src/main/java/com/mitranetpars/sportmagazine/MainActivity.java
+++ app/src/main/java/com/mitranetpars/sportmagazine/MainActivity.java
@@ -3,7 +3,7 @@ package com.mitranetpars.sportmagazine;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,9 +18,20 @@ public class MainActivity extends AppCompatActivity {
                 loginButton_onClick(v);
             }
         });
+
+        findViewById(R.id.signinButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signinButton_onCLick(v);
+            }
+        });
     }
 
-    public void loginButton_onClick(View v) {
-        Toast.makeText(MainActivity.this, "@string/login", Toast.LENGTH_LONG).show();
+    private void loginButton_onClick(View v) {
+
+    }
+
+    private void signinButton_onCLick(View v) {
+
     }
 }
